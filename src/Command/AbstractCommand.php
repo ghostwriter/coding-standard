7b73e08ef3625c98f61590ba9e69a072184ae0af
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-01 13:38:12 -0500

    Update AbstractCommand.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Command/AbstractCommand.php
+++ src/Command/AbstractCommand.php
@@ -6,11 +6,13 @@ namespace Ghostwriter\CodingStandard\Command;
 
 use Composer\Command\BaseCommand;
 use Composer\Util\ProcessExecutor;
+use Ghostwriter\CodingStandard\WindowsPathConverter;
 
 abstract class AbstractCommand extends BaseCommand
 {
     public function __construct(
         protected readonly ProcessExecutor $processExecutor,
+        protected readonly WindowsPathConverter $windowsPathConverter,
     ) {
         parent::__construct(null);
     }
