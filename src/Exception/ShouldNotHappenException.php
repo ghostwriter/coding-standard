c2d859d2201355fceee6b4b622c79ce4dcf3016d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-17 10:22:51 -0600

    Update ShouldNotHappenException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ShouldNotHappenException.php
+++ src/Exception/ShouldNotHappenException.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Ghostwriter\CodingStandard\Exception;
 
 use Ghostwriter\CodingStandard\Interface\CodingStandardExceptionInterface;
+use LogicException;
 
-final class ShouldNotHappenException  extends \LogicException implements CodingStandardExceptionInterface
-{
-}
+final class ShouldNotHappenException extends LogicException implements CodingStandardExceptionInterface {}
