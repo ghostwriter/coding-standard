3e04a625d1453c66bc7559cfcefa79ae0e4ac4af
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-17 10:21:21 -0600

    Update WindowsPathConverter.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/WindowsPathConverter.php
+++ src/WindowsPathConverter.php
@@ -4,25 +4,32 @@ declare(strict_types=1);
 
 namespace Ghostwriter\CodingStandard;
 
+use RuntimeException;
+
+use const DIRECTORY_SEPARATOR;
+
+use function defined;
+use function getcwd;
+use function str_replace;
+
 final readonly class WindowsPathConverter
 {
     /**
      * Convert a Unix-style path to a Windows-style path, on windows.
      *
-     * @param string $unixPath The Unix-style path to convert.
-     * @return string The Windows-style path.
+     * @param string $unixPath the Unix-style path to convert
+     *
+     * @return string the Windows-style path
      */
-    public function convert(
-        string $unixPath,
-    ): string
+    public function convert(string $unixPath): string
     {
-        if (!\defined('PHP_WINDOWS_VERSION_BUILD')) {
+        if (! defined('PHP_WINDOWS_VERSION_BUILD')) {
             return $unixPath;
         }
 
         $currentWorkingDirectory = getcwd();
-        if ($currentWorkingDirectory === false) {
-            throw new \RuntimeException('Could not determine the current working directory.');
+        if (false === $currentWorkingDirectory) {
+            throw new RuntimeException('Could not determine the current working directory.');
         }
 
         // Replace forward slashes with backslashes
@@ -30,5 +37,4 @@ final readonly class WindowsPathConverter
 
         return $currentWorkingDirectory . DIRECTORY_SEPARATOR . $windowsPath;
     }
-
 }
