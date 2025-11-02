0060817000d061fb9f7f91a46d6807f1f8bbc48e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-11-01 20:13:27 -0500

    Update packages
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- functions.php
+++ functions.php
@@ -3,13 +3,16 @@
 declare(strict_types=1);
 
 use Composer\Autoload\ClassLoader;
-use Ghostwriter\Filesystem\Filesystem;
 
 /** @var ClassLoader $classLoader */
 $classLoader = require __DIR__ . '/vendor/autoload.php';
 
-foreach (Filesystem::new()->recursiveIterator(__DIR__ . \DIRECTORY_SEPARATOR . 'functions') as $filePath) {
-    require $filePath->toString();
+foreach (glob(__DIR__ . \DIRECTORY_SEPARATOR . 'functions' . \DIRECTORY_SEPARATOR . '*.php') as $filePath) {
+    require $filePath;
+}
+
+foreach (glob(__DIR__ . \DIRECTORY_SEPARATOR . 'functions' . \DIRECTORY_SEPARATOR . '**'. \DIRECTORY_SEPARATOR . '*.php') as $filePath) {
+    require $filePath;
 }
 
 return $classLoader;
