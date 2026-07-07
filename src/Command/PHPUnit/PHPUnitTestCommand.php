612be9cdd4105079c3f03560528cdecd258cde39
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2026-07-06 22:59:43 -0500

    Update PHPUnitTestCommand.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Command/PHPUnit/PHPUnitTestCommand.php
+++ src/Command/PHPUnit/PHPUnitTestCommand.php
@@ -1,3 +1,4 @@
+@ -0,0 +1,30 @@
 <?php
 
 declare(strict_types=1);
@@ -5,13 +6,16 @@ declare(strict_types=1);
 namespace Ghostwriter\CodingStandard\Command\PHPUnit;
 
 use Ghostwriter\CodingStandard\Command\AbstractCommand;
+use Override;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+
+use const PHP_BINARY;
 
 #[AsCommand(name: 'ghostwriter:phpunit:test', description: "Run the project's PHPUnit test suite",)]
 final class PHPUnitTestCommand extends AbstractCommand
 {
+    #[Override]
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->writeln("Running the project's PHPUnit test suite...");
@@ -23,7 +27,9 @@ final class PHPUnitTestCommand extends AbstractCommand
                 '--colors=always',
                 '--do-not-cache-result',
                 '--stop-on-failure',
-            ],
+                '--log-junit',
+                $this->windowsPathConverter->convert('.cache/phpunit/junit.xml'),
+            ]
         );
     }
 }
